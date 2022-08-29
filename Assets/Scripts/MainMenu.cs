5ef65739b21a27f4a404a9fd6c5066fb0e0b6082
Kerman <grigorij.perfilin@mail.ru>
2022-08-29 14:53:09 +0300

    Далее начинаются проблемы со слиянием. (Начало работы с SceneManager и логикой главного меню)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -27,26 +27,39 @@ public class MainMenu : MonoBehaviour
     }
 
     public void NewGame() {
+        // Надо создавать новый .json файл типа Autosave.json.
+        // При этом в Awake загружаемой сцены устанавливать данные из Autosave.json 
         Debug.Log("Новая игра");
+        SceneManager.LoadScene(1);
     }
 
     public void Continue() {
+        // Если нет файла Autosave.json, то кнопка вне времени и пространства.
+        // Если есть, то загрузить сцену и в сцене установить данные из Autosave.json
         Debug.Log("Продолжить");
     }
 
     public void LoadGame() {
+        // Необходимо меню выбора сохранений.
+
+        // После выбора сохранения Autosave.json перезаписывается с данными выбранного сохранения.
+        // Далее происходит всё тоже, что и при кнопке Continue. 
         Debug.Log("Загрузить сохранение");
     }
 
     public void Settings() {
+        //Дебаг тут красноречив
         Debug.Log("Когда-нибудь они появятся, но скорее всего не сегодня...\nИ не завтра...\nИ не в этом году...\nИ не в этой жизни...");
     }
 
     public void Bonus() {
+        // Возможно стоит добавить какое-то меню с настройкой сцены...
         Debug.Log("Адекватным вход запрещён");
+        SceneManager.LoadScene(4);
     }
 
     public void Exit() {
+        // Выход
         Debug.Log("Выход");
         Application.Quit();
     }
